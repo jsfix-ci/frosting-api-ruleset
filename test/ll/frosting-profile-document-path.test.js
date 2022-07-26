f20a0ad5b74213ff2d1317181056cc85c5de341d
shonteag <shonte.amatogrill@gmail.com>
2022-07-26 13:28:59 -0400

    PLAT-3660: + frosting ruleset for path characters

---
+++ test/ll/frosting-profile-document-path.test.js
@@ -0,0 +1,132 @@
+'use strict';
+
+const {join} = require('path');
+const {expect} = require('chai');
+const {Spectral} = require('@stoplight/spectral');
+const {Document, Parsers} = require('@stoplight/spectral');
+
+
+const RULESET_FILE = join(__dirname, '../../rules/ll/frosting-profile-document-path.yaml');
+
+
+describe('frosting-profile-document-path ruleset', function () {
+
+  let spectral;
+
+  beforeEach(function () {
+
+    spectral = new Spectral();
+
+  });
+
+  describe('no-whitespace-or-hyphens', function () {
+
+    it('passes when there is no whitespace or hyphens', function (done) {
+
+      const doc = new Document(`
+        openapi: 3.0.2
+        paths:
+          /somepath:
+            get:
+              responses:
+                '200':
+                  content:
+                    application/vnd.api+json:
+                      schema:
+                        type: object
+                        required:
+                        - data
+                        properties:
+                          data:
+                            type: array
+                            items:
+                              type: integer
+      `, Parsers.Yaml);
+
+      spectral.loadRuleset(RULESET_FILE)
+        .then(() => {
+
+          return spectral.run(doc);
+
+        })
+        .then((results) => {
+
+          expect(results.length).to.equal(0);
+          done();
+
+        });
+
+    });
+
+    it('fails when the path includes whitespace or hyphens', function (done) {
+
+      const doc = new Document(`
+        openapi: 3.0.2
+        paths:
+          /somepath:
+            get:
+              responses:
+                '200':
+                  content:
+                    application/vnd.api+json:
+                      schema:
+                        type: object
+                        required:
+                        - data
+                        properties:
+                          data:
+                            type: array
+                            items:
+                              type: integer
+          /some bad path:
+            get:
+              responses:
+                '200':
+                  content:
+                    application/vnd.api+json:
+                      schema:
+                        type: object
+                        required:
+                        - data
+                        properties:
+                          data:
+                            type: array
+                            items:
+                              type: integer
+          /some-bad-path:
+            get:
+              responses:
+                '200':
+                  content:
+                    application/vnd.api+json:
+                      schema:
+                        type: object
+                        required:
+                        - data
+                        properties:
+                          data:
+                            type: array
+                            items:
+                              type: integer
+      `, Parsers.Yaml);
+
+      spectral.loadRuleset(RULESET_FILE)
+        .then(() => {
+
+          return spectral.run(doc);
+
+        })
+        .then((results) => {
+
+          expect(results.length).to.equal(2);
+          expect(results[0].code).to.equal('no-whitespace-or-hyphens');
+          expect(results[1].code).to.equal('no-whitespace-or-hyphens');
+          done();
+
+        });
+
+    });
+
+  });
+
+});

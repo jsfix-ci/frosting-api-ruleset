66079756910927e8d5d10ca7847788ca762deb41
shonteag <shonte.amatogrill@gmail.com>
2022-10-24 13:01:26 -0400

    PLAT-3909: + webpack + webpack config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+/* eslint-disable newline-before-return */
+/* eslint-disable padded-blocks */
+'use strict';
+
+const glob = require('glob');
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: glob.sync('./rules/**/*.js*').reduce(function gather(obj, el) {
+    obj[path.parse(el).name] = el;
+    return obj;
+  }, {}),
+  output: {
+    path: __dirname,
+    filename: './functions/[name].js',
+    libraryTarget: 'commonjs-module'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+
+      }
+    ]
+  }
+};
